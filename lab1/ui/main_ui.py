a05d2d131e208d64da96559180fc177aaac90357
Anatolii <anatoly.griboedov@gmail.com>
2021-09-28 23:21:50 +0300

    Window name fix

--- lab1/ui/main_ui.py
+++ lab1/ui/main_ui.py
@@ -13,7 +13,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
+        MainWindow.setObjectName("gLab")
         MainWindow.resize(1000, 737)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
